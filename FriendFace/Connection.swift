d08a840e5de057931b318531412560dbfc1fe6e1
Donghyun <markk628@gmail.com>
2020-03-08 15:42:06 -0700

    users can now search for people on search bar

---
+++ FriendFace/Connection.swift
@@ -0,0 +1,14 @@
+//
+//  Connection.swift
+//  FriendFace
+//
+//  Created by Mark Kim on 3/8/20.
+//  Copyright © 2020 Mark Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Connection: Codable {
+    var id: String
+    var name: String
+}
--- FriendFace/ViewController.swift
+++ FriendFace/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UISearchResultsUpdating {
+    var friends = [Friend]()
+    var filteredFriends = [Friend]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let search = UISearchController(searchResultsController: nil)
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Find a friend"
+        search.searchResultsUpdater = self
+        navigationItem.searchController = search
+        
+        DispatchQueue.global().async {
+            do {
+                let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let downloadedFriends = try decoder.decode([Friend].self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.friends = downloadedFriends
+                    self.filteredFriends = downloadedFriends
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredFriends.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let friend = filteredFriends[indexPath.row]
+        
+        cell.textLabel?.text = friend.name
+        cell.detailTextLabel?.text = friend.friends.map { $0.name }.joined(separator: ", ")
+        
+        return cell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredFriends = friends.matching(searchController.searchBar.text)
+        tableView.reloadData()
+    }
 }
 
